6acab2694841591f1d86ff74ea68948898edd573
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-28 12:51:19 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,4 +1,9 @@
 def reformat_languages(languages)
   new_hash = {}
-  languages.each do 
+  languages.each do |style, languages|
+    languages.each do |language, typehash|
+      new_hash[language] = typehash
+      new_hash[language][:style] = [style]
+    end
+  end
 end
